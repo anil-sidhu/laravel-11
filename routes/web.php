9ceb222e84f2f59bcdc20850aa01a4bc7f835e95
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-05-13 23:54:44 +0530

    done with routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test',function(){
     return view('home');
 });
+Route::view('/home','home');
+Route::redirect('/home','/');
+
+
+Route::get('/about/{name}', function ($name) {
+    echo $name;
+    return view('about',["name"=>$name]);
+});
+
