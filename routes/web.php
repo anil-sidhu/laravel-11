4bcfd4b7cec8cc9fecf7a04f75fca29e0fd0be0a
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-05-15 12:19:20 +0530

    worked on controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('user',[UserController::class,'getUser']);
+Route::get('about/{name}',[UserController::class,'aboutUser']);
 
+Route::get('admin-login',[UserController::class,'adminLogin']);
