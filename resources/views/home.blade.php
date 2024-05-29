c1b81d2e174c9ca6a66190a83bace7a030e31ff7
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-05-29 16:34:02 +0530

    start

--- resources/views/home.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <h1>Home Page</h1>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::view('home','home');
