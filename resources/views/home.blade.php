71d298c5c59f63fa4f2e23c6c64f097e53e6aae7
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-05-29 21:53:20 +0530

    done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Home page</h1>
+    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::view('home','home')->middleware('check1');
+
+Route::middleware('check1')->group(function(){
+    Route::view('about','about');
+    Route::view('list','about');
+    Route::view('contact','about');
+    Route::view('user','about');
+});
+
+
+
