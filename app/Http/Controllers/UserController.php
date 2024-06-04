cac1f7470299baed7fc1d16f29e19c11e8d00f15
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-04 16:12:16 +0530

    worked on laravel and mysql connection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-
 class UserController extends Controller
 {
-    // 
-    function user(){
+    //
+    function users(){
         return DB::select('select * from users');
-    } 
+    }
 }
