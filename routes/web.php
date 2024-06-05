e3ca6753d942d7f5252b30f40fbb7ae7b30bc27b
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-06 02:05:41 +0530

    worked on app

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('users',[UserController::class,'users']);
