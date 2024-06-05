159059f837e4f78f580d5398605dee6d1033f46c
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-05 19:11:45 +0530

    worked on db table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ class UserController extends Controller
 {
     //
     function users(){
-        return DB::select('select * from users');
+        $users= DB::select('select * from users');
+        return view('users',["users"=>$users]);
     }
 }
