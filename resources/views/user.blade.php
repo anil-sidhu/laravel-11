2ec89e81e3ba90c6ce73e599395d0f605a903d51
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-12 18:47:28 +0530

    worked on http client

---
+++ resources/views/user.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <h1>User Data</h1>
+
+    <ul>
+        <li>
+            <span>Name : </span><span><b>{{$data->name}}</b></span>
+        </li>
+        <li>
+            <span>Email : </span><span><b>{{$data->email}}</b></span>
+        </li>
+        <li>
+            <span>Phone : </span><span><b>{{$data->phone}}</b></span>
+        </li>
+        <li>
+            <span>website : </span><span><b>{{$data->website}}</b></span>
+        </li>
+
+    </ul>
+    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('students',[StudentController::class,'getStudents']);
+Route::get('user',[UserController::class,'getUser']);
+
