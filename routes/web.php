e92029d732b3bb6f6d015d405b71f715bff240b6
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-16 16:43:09 +0530

    worked on http request

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::post('user',[UserController::class,'post']);
-
-Route::view('form','user');
