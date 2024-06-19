dc806815ee8c5794026b152fc4538e46aec546d1
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-20 00:10:56 +0530

    locale

--- resources/views/display.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <img style="width:400px"  src="{{url('storage/'.$path)}}" >
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
--- resources/views/upload.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <h1>Upload file</h1>
-    <form action="upload" method="post" enctype="multipart/form-data" >
-        @csrf
-        <input type="file" name="file">
-        <button>Upload file</button>
-    </form>
-</div>
