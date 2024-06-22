bc15ac585f5f1d0148bbb46b696ef7651779ad05
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-22 23:28:09 +0530

    start with file upload

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h1>
-        {{__('about.aboutPage')}}
-       
-    </h1>
-    <h2> {{__("welcome.about")}}</h2>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::view("welcome","welcome");
 
-Route::middleware('SetLang')->group(function(){
-
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    
-    
-    // Route::get('/about/{lang}',function($lang){
-    //     App::setLocale($lang);
-    //     return view('about');
-    // });
-    
-    Route::view('about','about');
-    Route::get('setlang/{lang}',function($lang){
-        Session::put('lang',$lang);
-        return redirect('/');
-    });
-
-});
