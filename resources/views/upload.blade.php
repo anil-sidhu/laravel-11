bd4c63d172445c197407170b1240f2443889df33
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-23 14:21:01 +0530

    start with save data

--- resources/views/upload.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <h1>Upload files</h1>
-    <form action="" method="post" enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="files[]" multiple />
-        <button>Upload Files</button>
-    </form>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
 
 Route::view("/","welcome");
-Route::view('upload','upload');
-
-Route::post('upload',[UploadController::class,'upload']);
