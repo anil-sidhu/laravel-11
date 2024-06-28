ccaadb8a75bec4e6a8fd2706336a406ac7743c15
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-28 19:13:50 +0530

    worked

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ImageController;
 
 Route::view("/","welcome");
 
-Route::view('add','add-student');
 
-Route::post('add',[StudentController::class,'add']);
 
-Route::get('list',[StudentController::class,'list']);
+Route::view('upload','upload');
+Route::post('upload',[ImageController::class,'upload']);
+Route::get('list',[ImageController::class,'list']);
 
-Route::get('delete/{id}',[StudentController::class,'delete']);
-
-Route::get('edit/{id}',[StudentController::class,'edit']);
-
-Route::put("edit-student/{id}",[StudentController::class,'editStudent']);
-
-Route::get('search',[StudentController::class,'search']);
-
-Route::post('delete-multi',[StudentController::class,'deleteMultiple']);
