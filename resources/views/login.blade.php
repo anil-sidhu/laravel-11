fd1425e5f14844209f5ca4877796de1642748f43
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-06-29 14:57:42 +0530

    done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,13 +2,20 @@
     <x-slot name="title">Login Page</x-slot>
     <x-slot name="main">
 <h1>User Login</h1>
-<input type="text" placeholder="Enter name">
-<br/>
-<br>
-<input type="text" placeholder="Enter password">
-<br>
-<br>
-<button>Login</button>
+<div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text"  class="form-control" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword">
+    </div>
+  </div>
+<button type="button" class="btn btn-primary">Login</button>
+
 
     </x-slot>
 
