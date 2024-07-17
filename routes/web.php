4899ac8fec31953bb4b57d16ac4c017b18fd3dba
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-07-18 01:47:37 +0530

    worked on apple

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SellerController;
 
 Route::view("/","welcome");
-
-Route::get('list',[SellerController::class,'list']);
-Route::get('many-to-one',[SellerController::class,'manyToOne']);
 
 
